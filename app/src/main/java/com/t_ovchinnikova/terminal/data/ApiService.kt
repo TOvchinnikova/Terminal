89cc51e2133c98c7632d67427c9a510f88472732
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2024-02-26 09:28:52 +0500

    Loading data

---
+++ app/src/main/java/com/t_ovchinnikova/terminal/data/ApiService.kt
@@ -0,0 +1,9 @@
+package com.t_ovchinnikova.terminal.data
+
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=5NpJ7MHpagQhB6A84CU2ncc7OYSM0Dpl")
+    suspend fun loadBars(): Result
+}
---
+++ app/src/main/java/com/t_ovchinnikova/terminal/presentation/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.t_ovchinnikova.terminal.presentation
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.t_ovchinnikova.terminal.ui.theme.TerminalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TerminalTheme {
+                val viewModel: TerminalViewModel = viewModel()
+                val screenState = viewModel.state.collectAsState()
+                when(val currentState = screenState.value) {
+                    is TerminalScreenState.Content -> {
+                        Log.d("MainActivity", currentState.barList.toString())
+                    }
+                    is TerminalScreenState.Initial -> {
+
+                    }
+                }
+            }
+        }
+    }
+}
