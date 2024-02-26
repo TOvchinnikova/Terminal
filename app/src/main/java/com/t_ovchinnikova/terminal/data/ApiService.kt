0b478c8b52d930eee21a54baa43a4676b22f95c4
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2024-02-26 11:44:01 +0500

    Scaling

--- app/src/main/java/com/t_ovchinnikova/terminal/data/ApiService.kt
+++ app/src/main/java/com/t_ovchinnikova/terminal/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=5NpJ7MHpagQhB6A84CU2ncc7OYSM0Dpl")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=5NpJ7MHpagQhB6A84CU2ncc7OYSM0Dpl")
     suspend fun loadBars(): Result
 }
